713c04f4550d1f49caf7acbd48dbe277071c6f7c
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-04-21 11:15:40 +0200

    Use APP_BASE_HREF_PROVIDER in App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {APP_BASE_HREF_PROVIDER} from "./const/app.base-href.provider";
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    APP_BASE_HREF_PROVIDER
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
