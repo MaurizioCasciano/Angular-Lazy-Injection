eae7dbae388601ecd2337470fd8d7a446eb38c5f
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-04-21 09:53:44 +0200

    Lazy Component
    ng g c lazy/lazy

--- src/app/lazy/lazy.module.ts
+++ src/app/lazy/lazy.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LazyRoutingModule } from './lazy-routing.module';
+import { LazyComponent } from './lazy/lazy.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LazyComponent
+  ],
   imports: [
     CommonModule,
     LazyRoutingModule
